a12b22c11b08290a5500285b859c0911847204ad
Lorenz Walthert <lorenz.walthert@icloud.com>
2016-10-31 16:08:44 +0100

    adding arguments dir_out, file_out, title, line_nr ect. to allow for more flexibility.

---
+++ R/sum_str.R
@@ -0,0 +1,90 @@
+#' Summarize the code structure
+#'
+#' Create a summary of one or multiple code files by collapsing them into the
+#' section separators and their comments.
+#' @param dir_in A directory where the file(s) can be found.
+#' @param dir_out A directory where to print the output to. "" implies the
+#'   console.
+#' @param file_in The name of a file which should be summarized. \code{NULL} will
+#'   create a summary of all files in the directory.
+#' @param file_out A connection or character string naming the file to print to.
+#'   The argument is irrelevant if \code{output_dir} is set to "".
+#' @param file_out_extension An file extension for the file to be created.
+#' @param width The character width of the output.
+#' @param line_nr A boolean value that indicates whether the line numbers should
+#'   be printed along with the structure summary.
+#' @param separator A boolean value indicating whether the separating lines
+#'   should be reported along their comments or not.
+#' @param title A boolean value indicating whether the reported summary should
+#'   contain a title or not.
+#'
+#'
+#'
+#'
+#' @export
+#'
+sum_str <- function(dir_in = "./vignettes/",
+                    dir_out = dir_in,
+                    file_in = "example.R",
+                    file_out = NULL,
+                    file_out_extension = "",
+                    width = 50,
+                    line_nr = TRUE,
+                    separator = TRUE,
+                    title = TRUE) {
+
+##  ............................................................................
+## argument interaction
+  if (is.null(file_out)) {
+    file_out <- paste0("code_summary-",
+                       gsub("^(.*)\\..*$", "\\1", file_in, perl = TRUE),
+                       file_out_extension)
+  }
+
+##  ............................................................................
+##  get pattern
+
+  path <- paste0(dir_in, file_in)
+  lines <- readLines(con = path)
+  pos <- grep("^#+([[:space:]]){1,4}", lines) # extract candiates
+  # allow spaces in the beginning (deactivated)
+  # pos <- grep("^[[:space:]]*#+([[:space:]]){1,4}", lines)
+  pattern <-lines[pos]
+
+##  ............................................................................
+##  modify pattern according to arguments
+  if (separator == FALSE) {
+    remove <- grep("^#+\\s+[_|\\.|\\..\\s]+$", lines[pos], perl = TRUE)
+    pattern <- pattern[-remove]
+  }
+
+
+  if (!is.null(width)) {
+    pattern <- substring(pattern, 1, width)
+  }
+
+  if (line_nr == TRUE) {
+    pattern <- paste(pos, pattern, sep = "\t")
+  }
+
+  if (title == TRUE) {
+    pattern <- append(paste0("Table of contents for ", file_in), pattern)
+  }
+
+  if ("" %in% c(dir_out, file_out)) {
+    path_out <- ""
+  } else {
+    path_out <- paste(dir_out, file_out, sep = "/")
+  }
+
+
+##  ............................................................................
+##  output pattern
+  cat(pattern, file = path_out, sep = "\n")
+}
+
+# relace ^\s+(#+) with \1 in Rstudio to move all breaks to the left.
+# extensions
+# - multiple files in a directrory
+# - output to file
+
