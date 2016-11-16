adcce104ee57f95f3debf95ee39ff6520ee4e7d5
Lorenz Walthert <lorenz.walthert@icloud.com>
2016-11-16 21:41:13 +0100

    update master with checking out all updates regarding simple code separator insertion from devel

--- R/insert_breaks.R
+++ R/insert_breaks.R
@@ -1,29 +1,30 @@
 #' insert segment, section or subsection break
 #'
 #' A function designed to use as an RStudio
-#'   \href{https://rstudio.github.io/rstudioaddins/}{add-in} for structuring code.
-#'   There are three levels granularity
-#'   \itemize{
-#'     \item level 1 sections, which are high-level blocks that can be separated
-#'       as follows \cr    ### ______________________
-#'     \item level 2 sections, which are medium-level blocks that can be separated
-#'       as follows \cr    ##  ..............................................
-#'     \item level 1 sections, which are low-level blocks that can be separated
-#'       as follows \cr #   .. . . . . . . . . . . . . . . . . . . . . . . .}
-#'   For optimal use, we recommend specifying shortcuts in the add-in settings.
-#' @details The breaks characters (\code{___}, \code{...}, \code{. .}) were choosen
-#'   such that they reflect the level of granularity, namely \code{___} has a
-#'   much higher visual density than
-#'   \code{. .} \cr
-#'   We recommend to start off grouping code into level 2 blocks.
-#'   The advantage is that in both directions of granularity,
-#'   there is another layer (\code{===} and \code{...}) left.
-#'   When the code base grows, there
-#'   might be the need to extend in both directions.
+#' \href{https://rstudio.github.io/rstudioaddins/}{add-in} for structuring code.
+#'  \cr
+#' There are three levels of granularity:
+#' \itemize{
+#'   \item level 1 sections, which are high-level blocks denoted by \cr
+#'     ### ______________________
+#'   \item level 2 sections, which are medium-level blocks denoted by \cr
+#'     ##  ..............................................
+#'   \item level 3 sections, which are low-level blocks denoted by \cr
+#'     #   .. . . . . . . . . . . . . . . . . . . . . . . .
+#' }
+#' For optimal use, we recommend specifying keyboard shortcuts in the add-in
+#' settings.
+#' @details The breaks characters (\code{___}, \code{...}, \code{. .}) were
+#'  chosen such that they reflect the level of granularity, namely \code{___}
+#'  has a much higher visual density than \code{. .} \cr
+#'  We recommend starting off by grouping code into level 2 blocks.
+#'  The advantage is that in both directions of granularity, there is another
+#'  layer (\code{___} and \code{...}) left. When the code base grows, there
+#'  might be a need to extend in both directions.
 #' @name insert_break
-#' @importFrom rstudioapi insertText
+#' @importFrom rstudioapi insertText getActiveDocumentContext setCursorPosition
 #' @examples
-#' # this is a minimal example.
+#' # This is a minimal example.
 #' # See the readme for a longer and more detailed example.
 #'
 #' ##  ......................................................
@@ -47,52 +48,121 @@
 #'
 #'
 #' ##  ......................................................
-
 NULL
 
+
+#   ____________________________________________________________________________
+#   exported functions
+##  ............................................................................
+##  level 1
 #' @rdname insert_break
 #' @export
 insert_l1_break <- function() {
-  to_insert <- help_insert(start = "#",
+  to_insert <- help_create_break(start = "#",
                            break_char = "_",
                            sep = "   ")
-  insertText(to_insert)
+  help_insert(to_insert,
+              start_row = 1,
+              start_indention = Inf,
+              start_indention_margin = 0,
+              end_row = 2,
+              end_indention = Inf)
 }
 
+##  ............................................................................
+##  level 2
 #' @rdname insert_break
 #' @export
 insert_l2_break <- function() {
-  to_insert <- help_insert(start = "##",
+  to_insert <- help_create_break(start = "##",
                            break_char = ".",
                            sep = "  ")
-  insertText(to_insert)
+  help_insert(to_insert,
+              start_row = 1,
+              start_indention = Inf,
+              start_indention_margin = 0,
+              end_row = 2,
+              end_indention = Inf)
 }
 
+##  ............................................................................
+##  level 3
 #' @rdname insert_break
 #' @export
 insert_l3_break <- function() {
-  to_insert <- help_insert(start = "###",
+  to_insert <- help_create_break(start = "###",
                            break_char = ". ",
                            sep = " .")
-  insertText(to_insert)
+  help_insert(to_insert,
+              start_row = 1,
+              start_indention = Inf,
+              start_indention_margin = 0,
+              end_row = 2,
+              end_indention = Inf)
+
 }
 
 
 
-
-help_insert <- function(start = "##",
+#   ____________________________________________________________________________
+#   helper functions
+##  ............................................................................
+##  help_create_break
+# the idea of the helper function is to return a string of a line length that is
+# composed of the start character and the break_characters
+help_create_break <- function(start = "##",
                         break_char = "-",
-                        lenght = options()$strcode.char.lenght,
+                        length = options()$strcode.char.length,
                         sep = " ") {
 
-
-  paste(paste(start, "", sep = sep),
-        paste(rep(
-            break_char,
-            ceiling((lenght - nchar(start) - nchar(sep))/nchar(break_char))),
+  temp <-
+    paste(start, sep,
+        paste(rep(break_char,
+            # ceiling necessary because patern like ". ." will get cut before
+            # length
+            ceiling((length - nchar(start) - nchar(sep))/nchar(break_char))),
           collapse = ""),
-        "\n",
-        # start, paste(rep(" ", 4 - nchar(start)), collapse = ""),
-        # insert new line and corresponding comment structure
         sep = "")
+  substring(temp, 1, length) # truncate pattern to exacly length
 }
+##  ............................................................................
+##  help_insert
+# this funciton first gets the row in the active document, inserts a text x
+# one row below and jumps another row down
+#' help insert
+#'
+#' A helper function to insert text
+#' @param x An object to insert
+#' @param start_row the start row of the insertion
+#' @param start_indention The start position within the row
+#' @param start_indention_margin A margin (i.e. spaces) that will be added
+#'   at the target row before \code{x} is inserted.
+#' @param end_row the row where the cursor should be after the insertion
+#' @param end_indention The end position within the row
+help_insert <- function(x,
+                        start_row = 1,
+                        start_indention = Inf,
+                        start_indention_margin = 0,
+                        end_row = 2,
+                        end_indention = Inf) {
+  # get the row where the cursor is
+  current_row <- getActiveDocumentContext()$selection[[1]]$range$start[1]
+  # set the cursor to the very left of that row
+  setCursorPosition(c(current_row, Inf))
+
+  # insert end_row line breaks
+  insertText(paste(rep("\n", end_row), collapse = ""))
+
+  # insert the margin at the target row
+  insertText(c(current_row  + start_row, start_indention),
+             paste(rep(" ", start_indention_margin), collapse = ""))
+
+
+  # insert the separator at the beginning of the new line, so \n gets
+  # shifted down one
+  insertText(c(current_row  + start_row, start_indention), x)
+  # move the cursor one line down
+  setCursorPosition(c(current_row + end_row, end_indention), id = NULL)
+
+}
+
