d554620eed31d26f0fa227d87c19a4683f84471f
Lorenz Walthert <lorenz.walthert@icloud.com>
2016-12-02 11:50:53 +0100

    remove not yet used option strcode.section.title

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
   op.strcode <- list(
-    strcode.char.length = 80,
-    strcode.section.title = FALSE
+    strcode.char.length = 80
   )
   toset <- !(names(op.strcode) %in% names(op))
   if(any(toset)) options(op.strcode[toset])
 
   invisible()
 }
-
