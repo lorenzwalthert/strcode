4f398f6c2c492eb40cf01c988ead3922b2051e24
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-01-08 23:06:32 +0100

    major updates
    major changes:
    - do not on-the-fly subsetting of the input *lines*, anymore but only collect indices and subset in the end. Hence, remove intermediate variables such as pattern and pos.
    - removed unnecessary functions get_gran_pattern and update_pos_pattern, which simplifies things.
    - move find_gran helper function out of sum_str_helper, add assertive test so infinite recursion loop can be avoided.
    - add documentation for helper functions. Resolved problem of where to write output to a file if file_in was located in subdirectory #142.
    - some regex-pattern corrections for consistency.
    - few documentation adjustements
    - change argument order. Now, all *in* arguments are first, then all *out* arguments.
    - print the name of the currently processed file and change warnings to be fed back to the user immediately, so they make more sense if multiple files are processed.

--- R/sum_str.R
+++ R/sum_str.R
@@ -3,14 +3,14 @@
 #' Create a summary of one or multiple code files based on the section
 #' separators and their comments.
 #' @param dir_in The directory where the file(s) can be found.
-#' @param dir_out The directory to print the output to. "" implies the console.
 #' @param file_in The name of a file which should be summarized. If this is
 #'   \code{NULL}, the summary will be for all files in the specified directory.
 #'   The default value uses the RStudio API to produce a summary of content from
 #'   the source editor. This requires that
 #'   the file is saved before \code{sum_str} is called.
 #' @param file_in_extension If \code{file_in} is \code{NULL}, all files with the
-#'   \code{file_in_extension} are considered.
+#'   \code{file_in_extension} are considered, defaults to ".R".
+#' @param dir_out The directory to print the output to. "" implies the console.
 #' @param file_out A connection or character string naming the file to print to.
 #'   The argument is irrelevant if \code{output_dir} is set to "".
 #' @param file_out_extension A file extension for the file to be created.
@@ -21,7 +21,7 @@
 #' @param granularity Indicates the lowest level of granularity that should be
 #'   included in the summary.
 #' @param lowest_sep A boolean value indicating whether or not the separating
-#'   lines should be reported along with their comments.
+#'   lines of the lowest level should be printed.
 #' @param title A boolean value indicating whether the reported summary should
 #'   contain a title or not.
 #' @param header A boolean value indicating whether a column header should
@@ -68,9 +68,9 @@
 #   ____________________________________________________________________________
 #   user-function
 sum_str <- function(dir_in = NULL,
-                    dir_out = "",
                     file_in = getSourceEditorContext()$path,
                     file_in_extension = ".R",
+                    dir_out = "",
                     file_out = NULL,
                     file_out_extension = "",
                     width = NULL,
@@ -98,11 +98,17 @@ sum_str <- function(dir_in = NULL,
 
 ##  ............................................................................
 ##  call helper
+
+  # if output is not printed in the console, print a short summary.
+  if (dir_out != "") {
+    cat("The following files were summarized \n")
+  }
   lapply(all_files, function(g) {
     # pass all arguments as is except the file_in
+    cat(g, sep = " \n")
     sum_str_helper(dir_in = dir_in,
-                   dir_out = dir_out,
                    file_in = g,
+                   dir_out = dir_out,
                    file_out = file_out,
                    file_out_extension = file_out_extension,
                    width = width,
@@ -112,17 +118,31 @@ sum_str <- function(dir_in = NULL,
                    title = title,
                    header = header)
   })
-
-  # if output is not printed in the console, print a short summary.
-  if (dir_out != "") {
-    cat("The following files were summarized",
-            as.character(all_files), sep = "\n")
-  }
+  invisible() # avoid unnecessary NULL return
 }
 
 #   ____________________________________________________________________________
-#   helper function
-
+#   helper function: sum_str_helper
+#' helper function for code summarisation
+#'
+#' Function is called by \code{sum_str()} and returns summary of one code file.
+#' @inheritParams sum_str
+#' @param dir_in The directory where the file can be found.
+#' @param file_in The name of a file which should be summarized.
+#'   The default value uses the RStudio API to produce a summary of content from
+#'   the source editor. This requires that
+#'   the file is saved before \code{sum_str} is called.
+#' @details The core of the function is described best as follows: after a file
+#' was read in and stored in a vector *lines* whereas each element describes a
+#' line of code, the candidate lines (in the sense that they might be contained
+#' in the summary) were evaluated, their indices stored in *cand*. Next,
+#' various regex patterns are matched against *lines*. Then,
+#' after all tests were executed, the variable *remove* contains all indices
+#' that should be removed from *lines* before it is returned as the summary of
+#' the code file. Hence, applying \code{setdiff(cand, remove)} contains
+#' the subset of *lines* that we finally want to output.
+#' @keywords internal
+#' @import checkmate
 sum_str_helper <- function(dir_in,
                            dir_out,
                            file_in,
@@ -136,16 +156,15 @@ sum_str_helper <- function(dir_in,
                            header) {
 ##  ............................................................................
 ##  argument interaction
-
 ### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 ### get the file_out together
-if (is.null(file_out)) {
-  file_out <- paste0("code_summary-",
-                     gsub("^(.*)\\..*$", "\\1", file_in, perl = TRUE),
-                     file_out_extension)
-}
-
-
+  if (is.null(file_out)) {
+    # file_out must be a file name. Grap the last expression after backslash.
+    backslash_rm <- gsub("^.*/(.*)", "\\1", file_in, perl = TRUE)
+    file_out <- paste0("code_summary-",
+                       gsub("^(.*)\\..*$", "\\1", backslash_rm, perl = TRUE),
+                       file_out_extension)
+  }
 
 ### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 ### paths
@@ -162,7 +181,7 @@ if (is.null(file_out)) {
 
   # path_out
   ## path_out is "" if dir_out is ""
-  if (is.null(dir_out) || dir_out == "") {
+  if (dir_out == "") {
     path_out <- ""
   # otherwise it is composed of dir_out and file_out, if file_out
   # has a not empty value
@@ -170,116 +189,124 @@ if (is.null(file_out)) {
     path_out <- paste(dir_out, file_out, sep = "/")
   }
 
-
-
-##  ............................................................................
-## function definitions
-  # find maximal or minimal level of granularity used.
-  find_gran <- function(direction = "up") {
-    if (direction == "up") {
-      l <- 1 # initialize
-    } else if (direction == "down") {
-      l <- 3
-    }
-
-    helper_find_gran <- function(direction) {
-      if (direction == "up") {
-        m <- 1
-      } else if (direction == "down") {
-        m <- -1
-      }
-      pattern <- paste0("^", paste0(rep("#", l), sep = "", collapse = ""), "\\s+")
-      if (any(grepl(pattern, lines, perl = TRUE))) {
-        l
-
-      } else {
-        l <<- l + m * 1
-        helper_find_gran(direction = direction)
-      }
-    }
-
-    helper_find_gran(direction)
-  }
-
 ##  ............................................................................
 ##  get pattern
 
   lines <- readLines(con = path_in)
-  sub_pattern <- "^#   |##  |### "
-  pos <- grep(sub_pattern, lines, perl = FALSE) # extract candiates
-  pattern <-lines[pos]
+  sub_pattern <- "^#   |^##  |^### "
+  cand <- grep(sub_pattern, lines, perl = FALSE) # extract candiates id
+  if (length(cand) == 0) {
+    return(warning("No line matching the required pattern",
+                   call. = FALSE, immediate. = TRUE))
+  }
 
 ##  ............................................................................
 ##  modify pattern according to arguments
-
 ### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 ### getting the granularity right
+  remove <- c()
+  if (granularity < 3) { # if there are any lines to remove
+    hashes <- (granularity + 1):3
+    spaces <- 4 - hashes
 
-  # remove the l lowest pattern separator depending on granularity if
-  # lowest_sep is TRUE
+    # this variable stores the indices of all lines that should be dropped.
+    for (i in 1:length(hashes)) {
+      sub_pattern <- paste0("^#{", hashes[i], "}\\s{", spaces[i], "}.*$")
+      remove <- append(remove, grep(sub_pattern, lines, perl = TRUE))
+    }
+  }
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### remove lowest separator
   if (lowest_sep == FALSE) {
-   sub_pattern <- paste0("^#{", min(granularity, find_gran("down")), ",", 4,
-                          "}\\s+[_|\\.].*$")
-   remove <- grep(sub_pattern, lines[pos], perl = TRUE)
-   pattern <- pattern[-remove]
-   pos <- pos[-remove]
+    hashes <- min(find_gran("down", lines = lines), granularity)
+    spaces <- 4 - hashes
+    sub_pattern <- paste0("^#{", hashes, "}\\s{", spaces, "}[\\._].*$")
+    remove <- append(remove, grep(sub_pattern, lines, perl = TRUE))
   }
-
-  # removing the l lowest separator comments and breaks depending on granularity
-  get_gran_pattern <- function(level = 3) {
-    paste("^", "#", "{", 1, ",", level, "}",
-          "\\s{", 1, ",", level, "}", sep = "", collapse = "")
-  }
-
-  update_pos_pattern <- function(level) {
-    keep <- grep(get_gran_pattern(level = level), lines[pos], perl = TRUE)
-    pattern <<- pattern[keep]
-    pos <<- pos[keep]
-
-  }
-
-  update_pos_pattern(granularity)
 
 ##  ............................................................................
-##  width adjust line_nr, title, output path, header
-  # only continue if there is a valid pattern
-  if (identical(pattern, character(0))) {
-    return(message("No line matching the required pattern"))
-  }
+##  select elements that "survived all tests"
+  tokeep <- setdiff(cand, remove)
+  lines <- lines[tokeep]
 
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### width adjust line_nr, title, output path, header
+
+  # only continue if there is a valid pattern
+  if (length(lines) == 0) {
+    return(warning("No line matching the required pattern",
+                   call. = FALSE, immediate. = TRUE))
+  }
   # adjust length of pattern.
   if (is.null(width)) {
   # first calculate width. It is the length of the maximal comment string
   ## get the comment strings
-    pattern_comments <- grep("^(#   |##  |### )[^._)].*$", pattern, value = TRUE)
+    pattern_comments <- grep("^(#   |##  |### )[^\\._)].*$", lines, value = TRUE)
     width <- max(nchar(pattern_comments))
   }
 
-  pattern <- substring(pattern, 1, width)
-
+  lines <- substring(lines, 1, width)
 
   if (line_nr == TRUE) {
-    pattern <- paste(pos, pattern, sep = "\t")
+    lines <- paste(tokeep, lines, sep = "\t")
   }
 
   if (header == TRUE) {
-    pattern <- append(c("line  level section"), pattern)
+    lines <- append(c("line  level section"), lines)
   }
   if (title == TRUE) {
-    pattern <- append(paste0("Summarized structure of ", file_in, "\n"), pattern)
+    lines <- append(paste0("Summarized structure of ", file_in, "\n"), lines)
   }
-
-
 
 ##  ............................................................................
 ##  output the pattern
-  cat(pattern, file = path_out, sep = "\n")
+  cat(lines, file = path_out, sep = "\n")
 }
 
- # relace ^\s+(#+) with \1 in Rstudio to move all breaks to the left.
- # extensions
- # - multiple files in a directrory
- # - output to file
+#   ____________________________________________________________________________
+#   helper function: find_gran
+# find maximal or minimal level of granularity used.
+#' Find out granularity of a string vector
+#'
+#' This helper function takes a string (vector) as an input and searches for the
+#' highest or lowest granularity. Granularity is defined in terms of a hash/
+#' space sequence (see \code{\link{sum_str}}). The search is implemented using
+#' a recursive approach.
+#' @param direction either "up" or "down". Down means the algorithm starts with
+#' the pattern containing a certain number of hashes (specified in
+#' \code{highest}) and searches for a regex match. If it can't find one, it
+#' reduces the number of hashes by one and searches again, until it finds a
+#' pattern.
+#' @param highest The highest level of granularity to search for.
+#' @param lowest The lowest level of granularity to search for.
+#' @param lines a character vector containing the lines to match against.
+#' @examples
+#'   strcode:::find_gran("down", highest = 3, lowest = 1, lines = c("##  .", "#   _"))
+#' @keywords internal
+find_gran <- function(direction = "down", highest = 3, lowest = 1, lines) {
+  # direction
+  if (direction == "up") {
+    current <- lowest
+    m <- 1
+  } else if (direction == "down") {
+    current <- highest
+    m <- -1
+  }
+
+    helper_find_gran <- function(direction) {
+    assert_number(current, lower = lowest, upper = highest)
+    pattern <- paste0("^", paste0(rep("#", current), collapse = ""),
+                      "\\s{", highest + 1 - current, "}.*$")
+    if (any(grepl(pattern, lines, perl = TRUE))) {
+      current
+    } else {
+      current <<- current + m * 1
+      helper_find_gran(direction = direction)
+    }
+  }
+  helper_find_gran(direction)
+}
 
 
 
