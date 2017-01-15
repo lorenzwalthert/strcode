b7cd1ba4fa5991f79f4ba47870012eeba70d335d
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-01-15 16:49:16 +0100

    add more testthat tests and add testthat to description file

---
+++ tests/testthat/raw_in/example-3.R
@@ -0,0 +1,8 @@
+# this file lacks level 1 granularity
+
+##  ............................................................................
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+##   a title that should not be like that, but displayed anyways.
+exp(1 + 10)
+
---
+++ tests/testthat/test-granularity.R
@@ -0,0 +1,153 @@
+# This file checks all combinations of existing granularity in a document and
+# requested granularity of the summary for consistency.
+
+#   ____________________________________________________________________________
+context("granularity example-1")
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 3
+
+# sum_str(file_in = "example-1.R", dir_in = "tests/testthat/raw_in", granularity = 3,
+# dir_out = "tests/testthat/correct", file_out = "out-ex-1_gran-3", title = FALSE)
+correct <- readLines("correct/out-ex-1_gran-3")
+test    <- sum_str(file_in = "raw_in/example-1.R", dir_out = "", granularity = 3,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 2
+# sum_str(file_in = "example-1.R", dir_in = "tests/testthat/raw_in", granularity = 2,
+# dir_out = "tests/testthat/correct", file_out = "out-ex-1_gran-2", title = FALSE)
+correct <- readLines("correct/out-ex-1_gran-2")
+test    <- sum_str(file_in = "raw_in/example-1.R", dir_out = "", granularity = 2,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 2
+# sum_str(file_in = "example-1.R", dir_in = "tests/testthat/raw_in", granularity = 1,
+# dir_out = "tests/testthat/correct", file_out = "out-ex-1_gran-1", title = FALSE)
+correct <- readLines("correct/out-ex-1_gran-1")
+test    <- sum_str(file_in = "raw_in/example-1.R", dir_out = "", granularity = 1,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+#   ____________________________________________________________________________
+context("granularity example-2")
+# create originals
+# mapply(sum_str,
+#        file_in = "example-2.R", dir_in = "tests/testthat/raw_in",
+#        dir_out = "tests/testthat/correct", title = FALSE,
+#        granularity = 3:1,
+#        file_out = paste0("out-ex-2_gran-", 3:1))
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 3
+correct <- readLines("correct/out-ex-2_gran-3")
+test    <- sum_str(file_in = "raw_in/example-2.R", dir_out = "", granularity = 3,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 2
+correct <- readLines("correct/out-ex-2_gran-2")
+test    <- sum_str(file_in = "raw_in/example-2.R", dir_out = "", granularity = 2,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 2
+correct <- readLines("correct/out-ex-2_gran-1")
+test    <- sum_str(file_in = "raw_in/example-2.R", dir_out = "", granularity = 1,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+#   ____________________________________________________________________________
+context("granularity example-3")
+# create originals
+# mapply(sum_str,
+#        file_in = "example-3.R", dir_in = "tests/testthat/raw_in",
+#        dir_out = "tests/testthat/correct", title = FALSE,
+#        granularity = 3:1,
+#        file_out = paste0("out-ex-3_gran-", 3:1))
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 3
+correct <- readLines("correct/out-ex-3_gran-3")
+test    <- sum_str(file_in = "raw_in/example-3.R", dir_out = "", granularity = 3,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 2
+correct <- readLines("correct/out-ex-3_gran-2")
+test    <- sum_str(file_in = "raw_in/example-3.R", dir_out = "", granularity = 2,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 1 not possible, level does not exist:
+# expect: warning
+expect_warning(sum_str(file_in = "raw_in/example-3.R", dir_out = "", granularity = 1,
+                       file_out = "object", title = FALSE))
+rm(list = ls())
+
+#   ____________________________________________________________________________
+context("granularity example-4")
+# generate examples
+# note that level 4 does not have any comments and level 2 is missing
+# mapply(sum_str,
+#        file_in = "example-4.R", dir_in = "tests/testthat/raw_in",
+#        dir_out = "tests/testthat/correct", title = FALSE,
+#        granularity = 3:1,
+#        file_out = paste0("out-ex-4_gran-", 3:1))
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 3
+correct <- readLines("correct/out-ex-4_gran-3")
+test    <- sum_str(file_in = "raw_in/example-4.R", dir_out = "", granularity = 3,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 2
+correct <- readLines("correct/out-ex-4_gran-2")
+test    <- sum_str(file_in = "raw_in/example-4.R", dir_out = "", granularity = 2,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 1
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 3
+correct <- readLines("correct/out-ex-3_gran-3")
+test    <- sum_str(file_in = "raw_in/example-3.R", dir_out = "", granularity = 3,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 2
+correct <- readLines("correct/out-ex-3_gran-2")
+test    <- sum_str(file_in = "raw_in/example-3.R", dir_out = "", granularity = 2,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
+
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+### check for granularity == 1
+# expect: warning
+correct <- readLines("correct/out-ex-4_gran-1")
+test    <- sum_str(file_in = "raw_in/example-4.R", dir_out = "", granularity = 1,
+                   file_out = "object", title = FALSE)[[1]]
+expect_identical(correct, test)
+rm(list = ls())
