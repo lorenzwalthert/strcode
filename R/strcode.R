3d3a8b28adc64581cb531892a9ea54035b16be71
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-02-14 23:01:00 +0100

    add vignette (which is the readme)

---
+++ R/strcode.R
@@ -0,0 +1,11 @@
+#' Structure and abstract your code better
+#'
+#' @description The \pkg{strcode} (short for structuring code) package contains
+#'   tools to organize and abstract your code better.
+#'
+#' @author Lorenz Walthert \email{lorenz.walthert@@icloud.com}, \cr
+#' @seealso
+#' See \code{vignette("strcode")} for an overview of the package.
+#' @docType package
+#' @name strcode
+"_PACKAGE"
