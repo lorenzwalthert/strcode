70095a779089298fa5e775ddf5f1016c857402b8
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-07-13 11:09:21 +0200

    change encl function

--- R/utils.R
+++ R/utils.R
@@ -136,15 +136,19 @@ create_fill <- function(title = "title",
                         id = "fj3",
                         classes,
                         attributes,
+                        json_ld = FALSE,
                         function_container) {
+  if (json_ld) {
+    fun <- paste
+  } else {
+    fun <- encl
+  }
+
   with(function_container, paste(
     get_title(title),
-    encl(get_id(id),
+    fun(get_id(id),
          papply(classes, get_class),
          papply(rm_space(attributes), get_attribute))
     )
   )
 }
-
-
-
